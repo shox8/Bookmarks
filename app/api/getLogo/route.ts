9c2ec5b14464a978f0e032b6c5d4cec595ec4076
Shoxruh <138918930+Shox2008@users.noreply.github.com>
2024-07-11 16:49:29 +0500

    Get Logo Route Added

---
+++ app/api/getLogo/route.ts
@@ -0,0 +1,25 @@
+import { NextRequest, NextResponse } from "next/server";
+
+const GETTER = require("get-website-favicon");
+
+interface Icons {
+  src: string;
+  sizes: string;
+  type: string;
+  origin: string;
+  rank: number;
+}
+
+type WebData = {
+  url: string;
+  baseUrl: string;
+  originUrl: string;
+  icons: Icons[];
+};
+
+export async function POST(request: NextRequest) {
+  const data = await request.json();
+  const { icons }: WebData = await GETTER(data);
+
+  return NextResponse.json(icons[0].src);
+}
